4fc221e1d1f4a7fde4c43a9472f94511c5851cf1
maui <maui@devf.mx>
2020-06-24 19:43:24 -0500

    Finalizar ejemplo de peticion con API Key; englobar peticiones en funciones

--- 01-Repaso/index.js
+++ 01-Repaso/index.js
@@ -46,12 +46,40 @@ const request = require('request');
 //   */
 // });
 
-const BR_BA_QUOTES = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes';
-request.get(BR_BA_QUOTES, (err, res, body) => {
-  if (res.statusCode === 200) {
-    const json = JSON.parse(body);
-    console.log(json);
-    console.log(json[0].quote);
-  }
-  else console.log(res.statusCode, err);
-});
+// 1) https://github.com/shevabam/breaking-bad-quotes
+//  -> No maneja auth;
+const getBreakingBadQuote = () => {
+  const BR_BA_QUOTES = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes';
+  request.get(BR_BA_QUOTES, (err, res, body) => {
+    if (res.statusCode === 200) {
+      const json = JSON.parse(body);
+      console.log(json);
+      console.log(json[0].quote);
+    }
+    else console.log(res.statusCode, err);
+  })
+};
+
+// getBreakingBadQuote();
+
+
+// 2) https://www.theaudiodb.com/api_guide.php
+//  -> Maneja Auth con API KEY
+
+const getBandInfo = (band) => {
+  const AUDIO_DB = `https://theaudiodb.com/api/v1/json/miLlaveUnica/search.php?s=${band}`;
+  request.get(AUDIO_DB, (err, res, body) => {
+    if (err) console.log(err);
+    if (res.statusCode === 200) {
+      const json = JSON.parse(body);
+      console.log(json.artists[0].strArtist);
+    }
+    else console.log(res.statusCode, err);
+  });
+}
+
+getBandInfo('Simon and Garfunkel');
+
+// 3) https://developer.tradier.com/
+//  -> Maneja Auth con OAuth
+
