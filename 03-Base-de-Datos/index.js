51dd9280337725243a31ebabb6bf2c3e82b8e009
maui <maui@devf.mx>
2020-06-25 20:05:06 -0500

    Agregar cascaron de endpoint para crear un recurso (aqui el recurso se llamaria animal)

--- 03-Base-de-Datos/index.js
+++ 03-Base-de-Datos/index.js
@@ -6,5 +6,20 @@ const PORT = process.env.PORT || 3000;
 // Endpoints
 api.get('/', (req, res) => res.status(200).json({ message: "it's alive!" }));
 
+// Create
+api.post('/api/animales', (req, res) => {
+  // 1) Recibir el animal que se quiere crear desde el cliente
+  // 2) Pedirle a la base de datos que guarde el nuevo animal
+  // 3) Con la respuesta que recibamos de la base de datos, le respondemos al cliente
+  const animal = { id: 'A1', nombre: 'Firulais', edad: 4 };
+  res.status(201).json({ animal });
+});
+
+// Read
+
+// Update
+
+// Delete
+
 // Encender el servidor
 api.listen(PORT, () => console.log(`Listening on ${PORT}`));
