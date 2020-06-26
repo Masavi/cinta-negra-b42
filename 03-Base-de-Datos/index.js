89fe3a704fbc45105c41fb069cdc027a44a82caa
maui <maui@devf.mx>
2020-06-25 19:29:08 -0500

    Agregar uso de variable de entorno en PORT (sirve para el deployment)

--- 03-Base-de-Datos/index.js
+++ 03-Base-de-Datos/index.js
@@ -1,7 +1,7 @@
 // Configuración
 const express = require('express');
 const api = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Endpoints
 api.get('/', (req, res) => res.status(200).json({ message: "it's alive!" }));
