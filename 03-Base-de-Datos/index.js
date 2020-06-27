1b4accb7f83c500dd59bdeba3c803e27c6b1997a
maui <maui@devf.mx>
2020-06-26 20:03:40 -0500

    Instalar mongoose y conectarse a la base de datos de test en el cluster de Mongo Atlas

--- 03-Base-de-Datos/index.js
+++ 03-Base-de-Datos/index.js
@@ -1,7 +1,13 @@
 // Configuración
+const mongoose = require('mongoose');
 const express = require('express');
 const api = express();
 const PORT = process.env.PORT || 3000;
+
+// Conexion a Base de Datos
+mongoose.connect('mongodb://maui:abc123def@cintanegra-shard-00-00-efwi5.mongodb.net:27017,cintanegra-shard-00-01-efwi5.mongodb.net:27017,cintanegra-shard-00-02-efwi5.mongodb.net:27017/test?ssl=true&replicaSet=CintaNegra-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Database connected!'))
+  .catch(() => console.log('Error connecting to database...'));
 
 // Endpoints
 api.get('/', (req, res) => res.status(200).json({ message: "it's alive!" }));
