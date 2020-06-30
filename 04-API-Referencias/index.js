3243ca50340c4a7411aced1182d7fc6b3dde6124
maui <maui@devf.mx>
2020-06-29 20:22:25 -0500

    Agregar esquema y modelo de productos en su propia carpeta; pedir modelo en archivo index de proyecto

--- 04-API-Referencias/index.js
+++ 04-API-Referencias/index.js
@@ -1,3 +1,6 @@
 require('dotenv').config();
 require('./mongoClient/index.js');
 
+const Products = require('./models/Products');
+
+Products.find()
---
+++ 04-API-Referencias/models/Products.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+/*
+ 1) Crear una base de datos para un supermercado que pueda 
+    almacenar lo siguiente:
+    - Artículo
+        -Nombre (string)
+        -Precio (number)
+        -Existencias (number)
+    - Ticket
+        -subtotal (number)
+        -IVA (number)
+        -total (number)
+        -articulos (articulo)
+*/
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    default: 25,
+  },
+});
+
+const Products = mongoose.Model('Products', productSchema);
+
+module.exports = Products; 
