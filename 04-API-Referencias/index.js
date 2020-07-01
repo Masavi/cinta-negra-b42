2688be73e4c73c7e5bdea9b6688ce42d3edaf725
maui <maui@devf.mx>
2020-06-30 20:53:38 -0500

    Agregar 'path' para poder enviar un html al hacer GET a la raiz de nuestra API

--- 04-API-Referencias/index.js
+++ 04-API-Referencias/index.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 require('./mongoClient/index.js');
 const express = require('express');
 const api = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 api.use(express.urlencoded({ extended: true }));
 api.use(express.json({ extended: true }));
 
-api.get('/', (req, res) => res.json({ message: "It's alive!" }));
+api.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
 // CRUD Products
 api.use(require('./routes/ProductRoutes'));
